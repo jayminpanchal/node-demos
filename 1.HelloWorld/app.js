9ea08bd2d8ee8062bc572fe03a0a6761464c84be
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-22 23:30:57 +0530

    Created server and listening on port 127.0.0.1:3000

---
+++ 1.HelloWorld/app.js
@@ -0,0 +1,12 @@
+var http=require("http");
+var fs=require("fs");
+
+var server=http.createServer(function(req,res){
+  console.log('request made'+req.url);
+  res.writeHead(200,{'content-Type':'text/html'});
+  var myStream=fs.createReadStream(__dirname+"index.html",'utf8');
+  myStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("Server listening on 127.0.0.1:3000");
