767cee3f4cbc60c771c4b70f9604b6c1c518b26f
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-23 23:56:55 +0530

    --Formatted All the files

--- 2.JSONResponse/app.js
+++ 2.JSONResponse/app.js
@@ -1,22 +1,22 @@
-var http=require("http");
-var fs=require("fs");
+var http = require("http");
+var fs = require("fs");
 
-var server=http.createServer(function(req,res){
-  console.log('request made'+req.url);
-  res.writeHead(200,{'content-Type':'application/json'});
-  var jsonRes=[{
-    name:'abc',
-    age:12,
-    city:'ahmedabad'
-  },
-  {
-    name:'xyz',
-    age:13,
-    city:'mumbai'
-  },
-];
-res.end(JSON.stringify(jsonRes));
+var server = http.createServer(function (req, res) {
+    console.log('request made' + req.url);
+    res.writeHead(200, {'content-Type': 'application/json'});
+    var jsonRes = [{
+        name: 'abc',
+        age: 12,
+        city: 'ahmedabad'
+    },
+        {
+            name: 'xyz',
+            age: 13,
+            city: 'mumbai'
+        },
+    ];
+    res.end(JSON.stringify(jsonRes));
 });
 
-server.listen(3000,'127.0.0.1');
+server.listen(3000, '127.0.0.1');
 console.log("Server listening on 127.0.0.1:3000");
--- 3.BasicRouting/app.js
+++ 3.BasicRouting/app.js
@@ -1,19 +1,19 @@
-var http=require("http");
-var fs=require("fs");
+var http = require("http");
+var fs = require("fs");
 
-var server=http.createServer(function(req,res){
-  console.log('request made'+req.url);
-  if(req.url === '/home' || req.url === '/'){
-      res.writeHead(200,{'content-Type':'text/html'});
-      fs.createReadStream(__dirname+"/index.html",'utf8').pipe(res);
-  }else if(req.url === '/contact'){
-      res.writeHead(200,{'content-Type':'text/html'});
-      fs.createReadStream(__dirname+"/contact.html",'utf8').pipe(res);
-  }else{
-      res.writeHead(200,{'content-Type':'text/html'});
-      fs.createReadStream(__dirname+"/404.html",'utf8').pipe(res);
-  }
+var server = http.createServer(function (req, res) {
+    console.log('request made' + req.url);
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'content-Type': 'text/html'});
+        fs.createReadStream(__dirname + "/index.html", 'utf8').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {'content-Type': 'text/html'});
+        fs.createReadStream(__dirname + "/contact.html", 'utf8').pipe(res);
+    } else {
+        res.writeHead(200, {'content-Type': 'text/html'});
+        fs.createReadStream(__dirname + "/404.html", 'utf8').pipe(res);
+    }
 });
 
-server.listen(3000,'127.0.0.1');
+server.listen(3000, '127.0.0.1');
 console.log("Server listening on 127.0.0.1:3000");
