77f3bfac954128f6e7b8f33dd456b2835c508c37
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-25 00:14:58 +0530

    --Basic routing of simple get request

--- 5.Express/app.js
+++ 5.Express/app.js
@@ -5,8 +5,13 @@
 var express = require("express");
 
 var app = express();
+
 app.get('/', function (req, res) {
-    res.send('this is home page');
+    res.send('This the home page.');
+});
+
+app.get('/contact', function (req, res) {
+    res.send('This the contact page.');
 });
 
 app.listen(3000);
