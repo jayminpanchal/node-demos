2972a0004f5666dfd798d852709aa45a84449b88
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-24 11:43:41 +0530

    Condition for different url with html content

--- 3.BasicRouting/app.js
+++ 3.BasicRouting/app.js
@@ -3,19 +3,16 @@ var fs=require("fs");
 
 var server=http.createServer(function(req,res){
   console.log('request made'+req.url);
-  res.writeHead(200,{'content-Type':'application/json'});
-  var jsonRes=[{
-    name:'abc',
-    age:12,
-    city:'ahmedabad'
-  },
-  {
-    name:'xyz',
-    age:13,
-    city:'mumbai'
-  },
-];
-res.end(JSON.stringify(jsonRes));
+  if(req.url === '/home' || req.url === '/'){
+      res.writeHead(200,{'content-Type':'text/html'});
+      fs.createReadStream(__dirname+"/index.html",'utf8').pipe(res);
+  }else if(req.url === '/contact'){
+      res.writeHead(200,{'content-Type':'text/html'});
+      fs.createReadStream(__dirname+"/contact.html",'utf8').pipe(res);
+  }else{
+      res.writeHead(200,{'content-Type':'text/html'});
+      fs.createReadStream(__dirname+"/404.html",'utf8').pipe(res);
+  }
 });
 
 server.listen(3000,'127.0.0.1');
