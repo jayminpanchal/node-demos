092771564882637c1d2300f9bdb174cb6071ce64
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-30 23:59:25 +0530

    --solved error of express

--- 7.TemplateEngineHandlebar/app.js
+++ 7.TemplateEngineHandlebar/app.js
@@ -11,7 +11,7 @@ app.use('/assets', express.static('assets'));
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {'title': 'Demo Page'});
 });
 
 app.get('/contact', function (req, res) {
