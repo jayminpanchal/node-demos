7da5cf5c8b6796b98ca556b41f5da4345cce4ca0
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-01 00:22:10 +0530

    --Basic pug Js setup with index file

---
+++ 8.TemplateEnginePug/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jaymin on 11/24/2016.
+ */
+
+var express = require("express");
+var app = express();
+var hbs = require('express-handlebars');
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + "/views/"}));
+app.use('/assets', express.static('assets'));
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res) {
+    res.render('index', {'title': 'Demo Page', 'condition': false});
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
+app.listen(3000);
