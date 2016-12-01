4188872f3a549bba9791bdf6dd54661d76c916d9
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-02 00:44:19 +0530

    --Solved issue of errors in session

--- 9.ValidationSessions/app.js
+++ 9.ValidationSessions/app.js
@@ -5,28 +5,34 @@
 var express = require("express");
 var app = express();
 var hbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + "/views/"}));
+
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 
 app.set('view engine', 'hbs');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.get('/', function (req, res) {
-    res.render('index', {'title': 'Validation Testing', success: false, errors: req.session.errors});
+    res.render('index', {'title': 'Validation Testing', success: req.session.success, errors: req.session.errors});
     req.session.errors = null;
 });
 
 app.post('/submit', function (req, res, next) {
     req.check('email', 'Invali email address').isEmail();
-    req.check('password', 'Invalid password').isLength({min: 4}).equals(req.body.cpasswor);
+    req.check('password', 'Invalid password').isLength({min: 4}).equals(req.body.cpassword);
 
     var errors = req.validationErrors();
     if (errors) {
         req.session.errors = errors;
+        req.session.success = false;
+    } else {
+        req.session.success = true;
     }
     res.redirect("/");
 });
