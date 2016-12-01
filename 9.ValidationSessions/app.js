a72843f4c868510bddcc159a9bf3057a6fe2d988
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-02 00:19:57 +0530

    --Handlebar setup and added form to test validation

--- 9.ValidationSessions/app.js
+++ 9.ValidationSessions/app.js
@@ -4,10 +4,20 @@
 
 var express = require("express");
 var app = express();
+var hbs = require('express-handlebars');
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + "/views/"}));
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 
+app.set('view engine', 'hbs');
 
-app.useMa(expressValidator());
-app.use(expressSession({secret: 'max', saveUnitialized: false, resave:false}));
+app.use(expressValidator());
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
+
+app.get('/', function (req, res) {
+    res.render('index', {'title': 'Validation Testing', success: false, errors: req.session.errors});
+    req.session.errors = null;
+});
+
 app.listen(3000);
