92cb5a173806585fe69d252cc61ef186fa304144
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-03 14:59:00 +0530

    --Bootstrap css compoent with simple usage

--- 10.Bootstrap/app.js
+++ 10.Bootstrap/app.js
@@ -5,13 +5,12 @@
 var express = require("express");
 var app = express();
 var hbs = require('express-handlebars');
-
+app.use('/bootstrap', express.static(__dirname + '/bower_components/bootstrap/dist/')); // redirect bootstrap JS
+app.use('/jquery', express.static(__dirname + '/bower_components/jquery/dist/')); // redirect JS jQuery
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + "/views/"}));
-
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-    res.render('index', {'title': 'Validation Testing', success: req.session.success, errors: req.session.errors});
-    req.session.errors = null;
+    res.render('index', {'title': 'Bootstrap Demo'});
 });
 app.listen(3000);
