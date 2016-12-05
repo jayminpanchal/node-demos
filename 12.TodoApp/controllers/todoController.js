ea8f9321a2c08ba613bb975d066859395e12b65e
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-06 00:41:15 +0530

    Default data displaying in view file

--- 12.TodoApp/controllers/todoController.js
+++ 12.TodoApp/controllers/todoController.js
@@ -1,10 +1,10 @@
 /**
  * Created by jaymin on 12/6/2016.
  */
-
+var data = [{item: 'Task 1'}, {item: 'Task 2'}, {item: 'Task3'}];
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render(todo);
+        res.render(todo, {todos: data});
     });
     app.post('/todo', function (req, res) {
 
