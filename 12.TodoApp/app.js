de3c69fefc1c31941ee69a59621fb7416eb28b00
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-07 00:40:01 +0530

    --Solving issue of not loading js and css

--- 12.TodoApp/app.js
+++ 12.TodoApp/app.js
@@ -7,7 +7,7 @@ var todoController = require('./controllers/todoController');
 
 var app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use('/assets', express.static('public/assets'));
 
 todoController(app)
 app.listen(3000);
--- 12.TodoApp/controllers/todoController.js
+++ 12.TodoApp/controllers/todoController.js
@@ -5,10 +5,9 @@ var bodyParser = require('body-parser');
 var data = [{item: 'Task 1'}, {item: 'Task 2'}, {item: 'Task3'}];
 var urlEncoder = bodyParser.urlencoded({extended: false});
 
-
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render(todo, {todos: data});
+        res.render('todo', {todos: data});
     });
     app.post('/todo', urlEncoder, function (req, res) {
         data.push(req.body);
