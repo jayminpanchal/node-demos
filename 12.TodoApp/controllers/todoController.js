a61db9c1dca4956936f2a16fabb86aeb39478fe2
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-07 00:19:26 +0530

    --Simple insert in data array by ajax request and push

--- 12.TodoApp/controllers/todoController.js
+++ 12.TodoApp/controllers/todoController.js
@@ -1,13 +1,18 @@
 /**
  * Created by jaymin on 12/6/2016.
  */
+var bodyParser = require('body-parser');
 var data = [{item: 'Task 1'}, {item: 'Task 2'}, {item: 'Task3'}];
+var urlEncoder = bodyParser.urlencoded({extended: false});
+
+
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
         res.render(todo, {todos: data});
     });
-    app.post('/todo', function (req, res) {
-
+    app.post('/todo', urlEncoder, function (req, res) {
+        data.push(req.body);
+        res.json(data);
     });
     app.delete('/todo', function (req, res) {
 
---
+++ 12.TodoApp/public/assets/todo-list.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+});
