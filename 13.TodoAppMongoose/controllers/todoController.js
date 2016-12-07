14a557c58cba2480eb1e8e1da6efdf37c73c0fae
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-12-08 01:33:56 +0530

    --Created account in mlab.com and basic connection with mongodb

--- 13.TodoAppMongoose/controllers/todoController.js
+++ 13.TodoAppMongoose/controllers/todoController.js
@@ -2,6 +2,19 @@
  * Created by jaymin on 12/6/2016.
  */
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the Database
+mongoose.connect('mongodb://username:password@ds151917.mlab.com:51917/todo');
+
+//create schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+//Create Model
+var Todo = mongoose.model('Todo', todoSchema);
+
 var data = [{item: 'Task 1'}, {item: 'Task 2'}, {item: 'Task3'}];
 var urlEncoder = bodyParser.urlencoded({extended: false});
 
